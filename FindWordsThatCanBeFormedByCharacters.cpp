5be7e1c860cbeab178fc7755e90212765a41099f
ashleyjwu <155711021+ashleyjwu@users.noreply.github.com>
2024-03-07 17:42:40 -0800

    Update FindWordsThatCanBeFormedByCharacters.cpp

--- FindWordsThatCanBeFormedByCharacters.cpp
+++ FindWordsThatCanBeFormedByCharacters.cpp
@@ -1,6 +1,6 @@
 /*You are given an array of strings words and a string chars.
 A string is good if it can be formed by characters from chars (each character can only be used once).
-Return the sum of lengths of all good strings in words./*
+Return the sum of lengths of all good strings in words.*/
 
 class Solution {
 public:
