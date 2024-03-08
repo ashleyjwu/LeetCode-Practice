fed0cd71c3ef26f22112e5d2f7066996c6fe6eb1
ashleyjwu <155711021+ashleyjwu@users.noreply.github.com>
2024-03-07 17:43:59 -0800

    Update LinkedListCycle.cpp

--- LinkedListCycle.cpp
+++ LinkedListCycle.cpp
@@ -1,3 +1,7 @@
+/*Given head, the head of a linked list, determine if the linked list has a cycle in it.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
+Return true if there is a cycle in the linked list. Otherwise, return false.*/
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
