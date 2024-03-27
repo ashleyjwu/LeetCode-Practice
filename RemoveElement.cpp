3284f73731ff9b7958747a2ef6825af04f8ab944
ashleyjwu <155711021+ashleyjwu@users.noreply.github.com>
2024-03-26 18:08:11 -0700

    Create RemoveElement.cpp

---
+++ RemoveElement.cpp
@@ -0,0 +1,24 @@
+/*Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.*/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for(int i = 0; i<nums.size(); i++){
+            if(nums.at(i)!=val){
+                int temp = nums.at(i);
+                nums.at(i) = nums.at(k);
+                nums.at(k)= temp;
+                k++;
+            }
+        }
+        //test output
+        for(int i = 0; i<nums.size(); i++){
+            cout << nums.at(i);
+        }
+        return k;
+    }
+};
